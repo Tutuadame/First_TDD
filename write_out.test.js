21e48e272968853d2b7bb2b3daaa96355769268e
h048032 <kosasvaril@gmail.com>
2021-10-19 22:29:55 +0200

    Kesz tesztek, mixed, mixed long.

--- write_out.test.js
+++ write_out.test.js
@@ -1,44 +1,66 @@
 function write_out(){
-    var out = "Hello, ";
-
+    var out ="Hello, ";
     if(arguments.length == 0){
-        console.log(out+"my friend.");
+        //console.log(out+"my friend.");
         return out+"my friend.";
     }
     
     switch(arguments.length){
-        case 1: Name = arguments[0].toString();
-                
+        case 1:
+                Name = arguments[0].toString();    
                 if( Name == Name.toUpperCase()){
                     out = "HELLO, ";
-                    console.log(out+Name+".");
+                    //console.log(out+Name+".");
                     return out+Name+".";        
                 }else{                                       
-                    console.log(out+Name+".");
+                    //console.log(out+Name+".");
                     return out+Name+".";        
                 }
                 
-        default: 
-                for( let i = 0 ; i < arguments.length; i++){
-                    if( i != arguments.length-1 && !arguments[i].isUppercase){
-                        out = out + arguments[i]+", ";
-                    }else if(!arguments[i].isUppercase){
-                        out = out + "and "+arguments[i]+".";
-                    }
-
-                    out_2 = "HELLO, ";
-                    if( i != arguments.length-1 && arguments[i].isUppercase){
-                        out_2 = out_2 + arguments[i]+", ";
-                    }else if(arguments[i].isUppercase){
-                        out_2 = out_2 + "AND "+arguments[i]+".";
+        default:
+                low = [];
+                upper = [];
+                for(let i = 0 ; i < arguments.length; i++){
+                    Name = arguments[i].toString();
+                    if(Name == Name.toUpperCase()){
+                        upper.push(Name);
+                    }else{
+                        low.push(Name);
                     }
                 }
-                if(out_2 == "HELLO, "){
-                    console.log(out);
-                    return out;      
+
+                out = "Hello, ";
+                out_2 = "AND HELLO, ";
+                for(let i=0;i<low.length;i++){
+                    if(low.length == 1){
+                        out += low[i]+".";
+                        break;
+                    }
+                    if(i != low.length-1 && low.length != 1){
+                        out += low[i]+", ";
+                    }else if(low.length != 1){                       
+                        out += "and "+low[i]+".";                        
+                    }
+                }
+                //console.log(out);
+                for(let i=0;i<upper.length;i++){
+                    if(upper.length == 1){
+                        out_2 += upper[i]+".";
+                        break;
+                    }
+                    if(i != upper.length-1 && upper.length != 1){
+                        out_2 += upper[i]+", ";
+                    }else if(upper.length != 1){
+                        out_2 += "AND "+upper[i]+".";
+                    }
+                }
+
+                if(out_2 == "AND HELLO, "){
+                    //console.log(out);
+                    return out;
                 }else{
-                    console.log(out+out_2);
-                    return out+out_2;      
+                    //console.log(out+out_2);
+                    return out+out_2;
                 }
        }
 }
@@ -66,13 +88,13 @@ describe("Greetings", function(){
         expect(write_out(A)).toBe("HELLO, "+A+".");
     })
 
-//    test("should write out for uppercase, mixed",()=>{
-//        A = "JAY", B = "Sarah", C = "KEVIN";
-//        expect(write_out()).toBe("Hello, "+B+"."+"HELLO,"+A+" AND "+B+".");
-//    })
-//
-//    test("should write out for uppercase, mixed long",()=>{
-//        A = "JAY", B = "Sarah", C = "KEVIN";
-//        expect(write_out(A,B,C)).toBe("Hello, "+B+"."+"HELLO,"+A+" AND "+C+".");
-//    })
+    test("should write out for uppercase, mixed",()=>{
+        A = "JAY", B = "Sarah", C = "KEVIN";
+        expect(write_out(A,B,C)).toBe("Hello, "+B+"."+"AND HELLO, "+A+", AND "+C+".");
+    })
+
+    test("should write out for uppercase, mixed",()=>{
+        A = "JAY", B = "Sarah", C = "KEVIN", D = "Maya", E = "Charlotte";
+        expect(write_out(A,B,C,D,E)).toBe("Hello, "+B+", "+D+", and "+E+"."+"AND HELLO, "+A+", AND "+C+".");
+    })
 });
