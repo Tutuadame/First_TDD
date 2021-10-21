beb1a66bc519200ac1c3dc78661933db2c27902f
h048032 <kosasvaril@gmail.com>
2021-10-21 20:42:02 +0200

    Talan kesz!

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+function difference(arg,low_array, upper_array){
+    for(let i = 0 ; i < arg.length; i++){
+        Name = arg[i].toString();
+        if(Name == Name.toUpperCase()){
+            upper_array.push(Name);
+        }else{
+            low_array.push(Name);
+        }
+    }
+}
+
+function concatenate(the_string, the_array){
+    for(let i=0;i<the_array.length;i++){
+        if(the_array.length == 1){
+            the_string += the_array[i]+".";
+            break;
+        }
+        if(i != the_array.length-1 && the_array.length != 1){
+            the_string += the_array[i]+", ";
+        }else if(the_array.length != 1){                       
+            if(the_string[1] == the_string[1].toUpperCase()){
+                the_string += "AND "+the_array[i]+"!";
+            }else{
+                the_string += "and "+the_array[i]+".";                        
+            }
+            
+        }
+    }
+    return the_string;
+}
+
+function change_the_end(arg){
+    for(let i=arg[0].length; i>=0; i--){
+        if(arg[0][i] == ","){
+            Change_2 = arg[0].substring(i, arg[0].length);
+            arg[0] = arg[0].substring(0,i);
+            break;
+        }
+    }
+    Change_2 = Change_2.replace(","," and");
+    return "Hello, "+arg[0]+Change_2+".";
+}
+
 function write_out(){
     var out ="Hello, ";
     if(arguments.length == 0){        
@@ -7,66 +50,32 @@ function write_out(){
     switch(arguments.length){
         case 1:
                 if(arguments[0].includes(",")){
-                    
-                    for(let i=arguments[0].length; i>=0; i--){
-                        if(arguments[0][i] == ","){
-                            Change_2 = arguments[0].substring(i,arguments[0].length);
-                            arguments[0] = arguments[0].substring(0,i);
-                            break;
-                        }
-                    }
-                    Change_2 = Change_2.replace(","," and");
-                    return "Hello, "+arguments[0]+Change_2+".";
+                    out = change_the_end(arguments);
+                    return out;
                 }else{
                     Name = arguments[0].toString();    
                     if( Name == Name.toUpperCase()){
                         out = "HELLO, ";                    
-                        return out+Name+".";
+                        return out+Name+"!";
                     }else{                                                          
                         return out+Name+".";       
                     }
                 }                
         default:
-                low = [];
-                upper = [];
-                for(let i = 0 ; i < arguments.length; i++){
-                    Name = arguments[i].toString();
-                    if(Name == Name.toUpperCase()){
-                        upper.push(Name);
-                    }else{
-                        low.push(Name);
-                    }
-                }
+                low_2 = [];
+                upper_2 = [];
+                difference(arguments, low_2, upper_2);
 
                 out = "Hello, ";
                 out_2 = "AND HELLO, ";
-                for(let i=0;i<low.length;i++){
-                    if(low.length == 1){
-                        out += low[i]+".";
-                        break;
-                    }
-                    if(i != low.length-1 && low.length != 1){
-                        out += low[i]+", ";
-                    }else if(low.length != 1){                       
-                        out += "and "+low[i]+".";                        
-                    }
-                }
-                
-                for(let i=0;i<upper.length;i++){
-                    if(upper.length == 1){
-                        out_2 += upper[i]+"!";
-                        break;
-                    }
-                    if(i != upper.length-1 && upper.length != 1){
-                        out_2 += upper[i]+", ";
-                    }else if(upper.length != 1){
-                        out_2 += "AND "+upper[i]+"!";
-                    }
-                }
+                out = concatenate(out, low_2);
+                out_2 = concatenate(out_2, upper_2);
 
-                if(out_2 == "AND HELLO, "){                    
+                if(out_2 == "AND HELLO, "){    
+                    console.log(out);               ;
                     return out;
                 }else{
+                    console.log((out+" ")+out_2);
                     return (out+" ")+out_2;
                 }
             
--- write_out.test.js
+++ write_out.test.js
@@ -1,5 +1,37 @@
 const write_out = require('./index.js');
 
+function difference(arg,low_array, upper_array){
+    for(let i = 0 ; i < arg.length; i++){
+        Name = arg[i].toString();
+        if(Name == Name.toUpperCase()){
+            upper_array.push(Name);
+        }else{
+            low_array.push(Name);
+        }
+    }
+}
+
+function concatenate(the_string, the_array){
+    for(let i=0;i<the_array.length;i++){
+        if(the_array.length == 1){
+            the_string += the_array[i]+".";
+            break;
+        }
+        if(i != the_array.length-1 && the_array.length != 1){
+            the_string += the_array[i]+", ";
+        }else if(the_array.length != 1){                       
+            if(the_string[1] == the_string[1].toUpperCase()){
+                the_string += "AND "+the_array[i]+"!";
+            }else{
+                the_string += "and "+the_array[i]+".";                        
+            }
+            
+        }
+    }
+    return the_string;
+}
+
+
 describe("Greetings", function(){
 
     test("should write out for no input", () =>{  
@@ -13,24 +45,48 @@ describe("Greetings", function(){
     test("should write out for multiple input", () =>{
         out_local = "Hello, ";
         A="Béla",B="Endre";
-        
         out_local += A +", and "+B+".";
+
         expect(write_out(A,B)).toBe(out_local);
     });
 
     test("should write out for uppercase",()=>{
         A = "JAY";
-        expect(write_out(A)).toBe("HELLO, "+A+".");
+        expect(write_out(A)).toBe("HELLO, "+A+"!");
     })
 
     test("should write out for uppercase, mixed",()=>{
         A = "JAY", B = "Sarah", C = "KEVIN";
-        expect(write_out(A,B,C)).toBe("Hello, "+B+"."+" AND HELLO, "+A+", AND "+C+"!");
+        arg = [];
+        low = [];
+        upper = [];
+
+        arg.push(A);arg.push(B);arg.push(C);
+
+        difference(arg,low,upper);
+        result = "Hello, ";
+        result_2 = " AND HELLO, ";
+        result = concatenate(result, low);
+        result_2 = concatenate(result_2, upper);
+
+        expect(write_out(A,B,C)).toBe(result+result_2);
     })
 
-    test("should write out for uppercase, mixed",()=>{
+    test("should write out for uppercase, mixed long",()=>{
         A = "JAY", B = "Sarah", C = "KEVIN", D = "Maya", E = "Charlotte";
-        expect(write_out(A,B,C,D,E)).toBe("Hello, "+B+", "+D+", and "+E+"."+" AND HELLO, "+A+", AND "+C+"!");
+
+        arg = [];
+        low = [];
+        upper = [];
+
+        arg.push(A);arg.push(B);arg.push(C);arg.push(D);arg.push(E);
+
+        difference(arg,low,upper);
+        result = "Hello, ";
+        result_2 = " AND HELLO, ";
+        result = concatenate(result, low);
+        result_2 = concatenate(result_2, upper);
+        expect(write_out(A,B,C,D,E)).toBe(result+result_2);
     })
 
     test("should write out for single input separated with commas",()=>{
@@ -43,7 +99,6 @@ describe("Greetings", function(){
                 break;
             }
         }
-        
         Change = Change.replace(","," and");
         console.log(Change);
 
